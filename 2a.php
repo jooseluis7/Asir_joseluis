ac1f7b1d1f3c0799e87cda1e872e2a320ca5f67a
joseluis <joseluislopez1999@gmail.com>
2019-10-07 16:21:41 +0200

    Variables

--- 2a.php
+++ 2a.php
@@ -1,8 +1,23 @@
 <?php
-$a=2;
-$b=8;
+$a=22;
+$b=14;
+echo '<div>';
 echo 'Suma de '.$a.' + '.$b.' = ';
 echo $a+$b;
+echo '</div>';
 
+echo '<div>';
+echo 'Resta de '.$a.' - '.$b.' = ';
+echo $a-$b;
+echo '</div>';
 
+echo '<div>';
+echo 'Multiplicación de '.$a.' x '.$b.' = ';
+echo $a*$b;
+echo '</div>';
+
+echo '<div>';
+echo 'División de '.$a.' / '.$b.' = ';
+echo $a/$b;
+echo '</div>';
 ?>
