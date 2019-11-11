fed6782988511d1f43a4705cf3f9dee040a03cc7
joseluis <joseluislopez1999@gmail.com>
2019-11-11 15:49:11 +0100

    V1

---
+++ HTML.php
@@ -0,0 +1,60 @@
+<html>
+
+<head>
+<meta charset="UTF-8"/>
+<title>PAGINA HTML</title>
+</head>
+
+<body>
+
+<center>
+
+<h1>PROVINCIAS BAÑADAS POR EL CANTABRICO</h1>
+
+<img src="mapa.jpg" alt="Mapa Politico de España" width="400" height="300"/>
+
+</center>
+
+<h2><font size="6", color="brown"><b><u>
+Estas son todas las provincias que son bañadas por el Mar Cantabrico:
+</font>
+</b></u></h2>
+
+<form>
+	<input/>
+	<input type="submit"/>
+</form>
+<table border="1">
+<tr>
+<th><font size="4"> Comunidades </font></th>
+<th><font size="4"> Provincias </font></th>
+</tr>
+<tr>
+<td><font color="blue", size="4"> Galicia </font></td>
+<td><font color="blue", size="4"> A Coruña </font></td>
+</tr>
+<tr>
+<td><font color="blue", size="4"> Galicia </font></td>
+<td><font color="blue", size="4"> Lugo </font></td>
+</tr>
+<tr>
+<td><font color="F4BD02", size="4"> Asturias </font></td>
+<td><font color="F4BD02", size="4"> Asturias </font></td>
+</tr>
+<tr>
+<td><font color="red", size="4"> Cantabria </font></td>
+<td><font color="red", size="4"> Cantabria </font></td>
+</font>
+</tr>
+</table>
+<p><b><font color="E502F4", size="5">
+Como podemos ver las provincias que son bañadas por el Cantabrico son Lugo, A Coruña y Asturias y Cantabria (estas dos ultimas son uniprovinciales)
+</font>
+</b></p>
+
+<h3>Modulos de ASIR</h3>
+<p> <font color="grey"> ASIR1: PAR, ISO, GBD, FOL, LMS y FH </font></p>
+</br>
+<p><font color="green"> ASIR2: SRI, ASGBD, ASO, EIE, IAW, SAD </font></p>
+</body>
+</html>
