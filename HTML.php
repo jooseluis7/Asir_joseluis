887693466a5a65580bea39c58147a90100173d7a
joseluis <joseluislopez1999@gmail.com>
2019-12-02 15:45:40 +0100

    Atrasadas

--- HTML.php
+++ HTML.php
@@ -1,60 +1,64 @@
-<html>
-
-<head>
-<meta charset="UTF-8"/>
-<title>PAGINA HTML</title>
-</head>
-
-<body>
-
-<center>
-
-<h1>PROVINCIAS BAÑADAS POR EL CANTABRICO</h1>
-
-<img src="mapa.jpg" alt="Mapa Politico de España" width="400" height="300"/>
-
-</center>
-
-<h2><font size="6", color="brown"><b><u>
-Estas son todas las provincias que son bañadas por el Mar Cantabrico:
-</font>
-</b></u></h2>
-
-<form>
-	<input/>
-	<input type="submit"/>
-</form>
-<table border="1">
-<tr>
-<th><font size="4"> Comunidades </font></th>
-<th><font size="4"> Provincias </font></th>
-</tr>
-<tr>
-<td><font color="blue", size="4"> Galicia </font></td>
-<td><font color="blue", size="4"> A Coruña </font></td>
-</tr>
-<tr>
-<td><font color="blue", size="4"> Galicia </font></td>
-<td><font color="blue", size="4"> Lugo </font></td>
-</tr>
-<tr>
-<td><font color="F4BD02", size="4"> Asturias </font></td>
-<td><font color="F4BD02", size="4"> Asturias </font></td>
-</tr>
-<tr>
-<td><font color="red", size="4"> Cantabria </font></td>
-<td><font color="red", size="4"> Cantabria </font></td>
-</font>
-</tr>
-</table>
-<p><b><font color="E502F4", size="5">
-Como podemos ver las provincias que son bañadas por el Cantabrico son Lugo, A Coruña y Asturias y Cantabria (estas dos ultimas son uniprovinciales)
-</font>
-</b></p>
-
-<h3>Modulos de ASIR</h3>
-<p> <font color="grey"> ASIR1: PAR, ISO, GBD, FOL, LMS y FH </font></p>
-</br>
-<p><font color="green"> ASIR2: SRI, ASGBD, ASO, EIE, IAW, SAD </font></p>
-</body>
-</html>
+ <!DOCTYPE html>
+ 
+	<html>
+		<head>
+			<meta charset="UTF-8"/>
+			<title>Balonmano Cantabro</title>
+		</head>
+	<body bgcolor="CFFF37" marginwidth="50">
+			<h1> <font color="FFA437">Balonmano Cantabro</font></h1>
+			 <img src="https://pbs.twimg.com/profile_images/1201441875162521600/zbePMDC3_400x400.jpg"  width="300" height="300"/> <br/>
+			
+				<h2>El primer<u> equipo cantabro que veremos</u> <del> sera el mejor equipo de la capital</del></h2>
+				
+					<h3> Sin Fin DS autogomas </h3>
+						
+						<p> El unico equipo cantabro que esta en primera divion </p>
+					
+				<h2>En segundo lugar todos los equipos que estan en segunda divión</h2>
+					
+					<p> Hay tres tipos </p>
+						
+						<h4> Balonmano Torrelavega </h4>
+						<h4> Balonmano Santoña </h4>
+						<h4> Balonmano Pereda </h4>
+			
+			<h1><font color="MediumSeaGreen">Equipos Catalanes</font></h1>
+			
+				<h2>El <b>primero</b> el mas conocio <mark>a nivel mundial </mark></h2>
+				
+					<h3>Balonmano Barcelona</h3> <br/>
+				<center>
+				<form>
+					Nombre: <input type="text" name="fname"/><br/>
+					<input type="submit" value="Submit"/>
+				</form> 
+				<br/>
+				
+					<p><font size="5"> Jugadores más altos de asobal </font></p>
+					
+			 <table border width="250">
+				<tr align="center">
+					<th>Nombre</th>
+					<th> Estatura(m)</th>
+				</tr>
+				<tr align="center">
+					<td>Laszlo Nagy</td>
+					<td>2.09</td>
+				</tr>
+				<tr align="center">
+					<td>Ljubomir Pavlovic</td>
+					<td>2.09</td>
+				</tr>
+				<tr align="center">
+					<td>Alberto Val</td>
+					<td>2.08</td>
+				</tr>
+				<tr align="center">
+					<td>Nenad Biblija</td>
+					<td>2.07</td>
+				</tr>
+			</table> 	
+	</center>
+	</body>
+	</html> 
